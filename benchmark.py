5bfc13193cba1b2a8f2f79f4b9ba7906495978ca
Benoit Dequidt <benoit.dequidt@gmail.com>
2019-01-12 14:42:06 +0800

    Add benchmark.py to generate a offline html page
    Note it seems there are some bugs with plotly which
    is not grouping properly the legends....
    
    TODO use someting else to generate a one-off html page

--- benchmark.py
+++ benchmark.py
@@ -4,6 +4,10 @@ import argparse
 from pprint import pprint
 import collections
 
+import plotly
+import plotly.plotly as py
+import plotly.graph_objs as go
+
 def parse_benchmark_name(name: str):
     """
     Parses a template benchmark name with a size
@@ -78,6 +82,45 @@ def parse_benchmark_json(input: dict):
     return benchmarks
 
 
+def plot_benchmarks(benchmarks : dict()):
+    data = collections.OrderedDict()
+    plot_titles = list()
+    for name, benchmark in benchmarks.items():
+        x_values = [b.size for b in benchmark]
+        y_values = [b.cpu_time/b.size for b in benchmark]
+        params = benchmark[0].t_params
+        plot_key = benchmark[0].name
+        if len(params) == 3:
+            line_name = params[2] + '<' + params[0] + ', ' + params[1] + '>'
+            plot_key += '<' + params[0] + ', ' + params[1] + '>'
+        else:
+
+            line_name = params[1] + '<' + params[0] + ', Action<' + params[0] + '>>'
+            plot_key += '<' + params[0] + ', Action<' + params[0] + '>>'
+
+        trace = go.Scatter(
+            x = x_values,
+            y = y_values,
+            mode = 'lines+markers',
+            name = line_name,
+            legendgroup = name,
+        )
+        if not plot_key in data.keys():
+            data[plot_key] = list()
+            plot_titles.append(plot_key)
+        data[plot_key].append(trace)
+
+
+    fig = plotly.tools.make_subplots(rows=len(data.keys()), subplot_titles=plot_titles)
+    i = 1
+    for k, traces in data.items():
+        for t in traces:
+            fig.append_trace(t, i, 1)
+        i+= 1
+
+    fig['layout'].update(height=2800, width=1200, title='hashmap performance')
+    plotly.offline.plot(fig)
+
 if __name__ == '__main__':
     arg_parser = argparse.ArgumentParser(description='Parse google benchmark output')
     arg_parser.add_argument('file', help='input filename')
@@ -87,3 +130,4 @@ if __name__ == '__main__':
         data = json.load(f)
         benchmarks = parse_benchmark_json(data)
         pprint(benchmarks)
+        plot_benchmarks(benchmarks)
