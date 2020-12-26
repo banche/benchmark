4d2a09ab7e47b51d2425b7fa29b92afa4bf18edd
Benoit Dequidt <benoit.dequidt@gmail.com>
2020-12-26 12:31:07 +0800

    thirdparties: add fmt (required by folly) and add required options
        + configure.sh:
            - set fmt install mode
            - use ninja (not required)
        + also compile fmt

--- configure.sh
+++ configure.sh
@@ -2,5 +2,5 @@
 
 mkdir -p build
 pushd build
-cmake -DCMAKE_BUILD_TYPE=Release -DBENCHMARK_ENABLE_TESTING=OFF ../
+cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DBENCHMARK_ENABLE_TESTING=OFF -DFMT_INSTALL=ON ../
 popd
